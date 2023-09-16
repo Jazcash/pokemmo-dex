09dbac8c6fd46299391132df96ad0ee1c4ab0c76
Jazcash <jaspercashmore@gmail.com>
2023-09-17 00:34:49 +0100

    use relative dir for assets

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "",
     resolve: {
         alias: {
             "@": path.join(__dirname, "src"),
